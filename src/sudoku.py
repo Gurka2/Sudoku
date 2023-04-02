c3ac4d2dd28815ed3826bead5b0836810a57dd29
martin <marho227@student.liu.se>
2023-04-02 11:28:54 +0200

    redid my 'struct' and removed unnecessary comments in renderer

--- src/sudoku.py
+++ src/sudoku.py
@@ -9,6 +9,19 @@ from typing import Any
 # requirements
 import getch
 
+class Sudoku():
+    """ This class is actually a struct """
+
+    def __init__(self, board_pos_x, board_pos_y):
+        self.x = 1
+        self.board_pos = (board_pos_x, board_pos_y)
+        self.cursor_x = 8
+        self.cursor_y = 1
+        self.cursor_color = Color.CYAN
+        self.board = init_empty_board()
+        self.user_input = []
+        self.exit = False
+
 def calculate_abs_cursor_pos(cursor_relative_x: int,
                              cursor_relative_y: int,
                              board_pos:         tuple[int, int]
@@ -25,18 +38,19 @@ def calculate_abs_cursor_pos(cursor_relative_x: int,
 
     return (cursor_abs_x, cursor_abs_y)
 
-def handle_cursor_movement(game: dict[str, Any]):
+def handle_cursor_movement(game: object):
     """ 
     Moves the cursor around the board according to vim keybinds
     without going outside of the board
     """
-    user_input: list[str] = game['user_input']
+    user_input: list[str] = game.user_input
 
     # TODO: DRY
+    # TODO: Redraw after mouse moved
     if user_input[-1] == 'h':
 
         if len(user_input) == 1:
-            game['cursor_x'] = max(0, game['cursor_x'] - 1)
+            game.cursor_x = max(0, game.cursor_x - 1)
 
         else:
             number: str = ''
@@ -44,36 +58,36 @@ def handle_cursor_movement(game: dict[str, Any]):
             for value in user_input[0:-1]:
                 number += value
 
-            game['cursor_x'] = max(0, game['cursor_x'] - int(number))
+            game.cursor_x = max(0, game.cursor_x - int(number))
 
     if user_input[-1] == 'l':
 
         if len(user_input) == 1:
-            game['cursor_x'] = min(8, game['cursor_x'] + 1)
+            game.cursor_x = min(8, game.cursor_x + 1)
 
         else:
             number: str = ''
 
             for value in user_input[0:-1]:
                 number += value
-            game['cursor_x'] = min(8, game['cursor_x'] + int(number))
+            game.cursor_x = min(8, game.cursor_x + int(number))
 
     if user_input[-1] == 'j':
 
         if len(user_input) == 1:
-            game['cursor_y'] = min(8, game['cursor_y'] + 1)
+            game.cursor_y = min(8, game.cursor_y + 1)
 
         else:
             number: str = ''
 
             for value in user_input[0:-1]:
                 number += value
-            game['cursor_y'] = min(8, game['cursor_y'] + int(number))
+            game.cursor_y = min(8, game.cursor_y + int(number))
 
     if user_input[-1] == 'k':
 
         if len(user_input) == 1:
-            game['cursor_y'] = max(0, game['cursor_y'] - 1)
+            game.cursor_y = max(0, game.cursor_y - 1)
 
         else:
             number: str = ''
@@ -81,38 +95,30 @@ def handle_cursor_movement(game: dict[str, Any]):
             for value in user_input[0:-1]:
                 number += value
 
-            game['cursor_y'] = max(0, game['cursor_y'] - int(number))
+            game.cursor_y = max(0, game.cursor_y - int(number))
 
 def take_user_input() -> list[str]:
     # crashes on anything but esc, letters and number, e.g. ctrl-d => crash
     return [getch.getch()]
 
 def handle_user_input(game: dict[str, Any]):
-    user_input = game['user_input'][-1]
+    user_input = game.user_input[-1]
     
     if user_input in 'qp':
-        game['exit'] = True
+        game.exit = True
     
     if user_input in 'hjkl':
         handle_cursor_movement(game)
-        game['user_input'] = []
+        game.user_input = []
 
     if user_input in '123456789':
         return
 
 def start_sudoku(board_pos_x: int, board_pos_y: int):
 
-    # Init game variables, and this is a struct
-    game = {
-    'board_pos': (board_pos_x, board_pos_y),
-    'cursor_x': 8,
-    'cursor_y': 1,
-    'cursor_color': Color.CYAN,
-    'board': init_empty_board(),
-    'user_input': [],
-    'exit': False}
-
-    solve_sudoku(game['board'])
+    # Init game variables
+    game = Sudoku(board_pos_x, board_pos_y)
+    solve_sudoku(game.board)
 
     hide_cursor()
 
@@ -121,16 +127,19 @@ def start_sudoku(board_pos_x: int, board_pos_y: int):
 
         # Graphics
         clear_terminal()
-        render_board_at_pos(game['board'], game['board_pos'])
-        cursor_abs_pos = calculate_abs_cursor_pos(game['cursor_x'], game['cursor_y'], game['board_pos'])
-        render_cursor_pos(cursor_abs_pos, game['cursor_color'])
+        render_board_at_pos(game.board, game.board_pos)
+        cursor_abs_pos = calculate_abs_cursor_pos(game.cursor_x,
+                                                  game.cursor_y,
+                                                  game.board_pos)
+        render_cursor_pos(cursor_abs_pos, game.cursor_color)
 
-        game['user_input'] += take_user_input()
+        game.user_input += take_user_input()
         handle_user_input(game)
 
-        if game['exit']:
+        if game.exit:
             break
 
     show_cursor()
+    clear_terminal()
 
 
